6e1bccaebbddb61418606f2afca11955bc2f2fe1
Deepak Kumar Mandal <mdeepak.be16@gmail.com>
2024-02-11 00:52:32 +0530

    Authenticatio token and roles done

--- routes/courses.js
+++ routes/courses.js
@@ -9,7 +9,10 @@ const {
 
 const Course = require('../models/Course');
 const advancedResults = require('../middleware/advancedResults');
+
 const router = express.Router({ mergeParams: true });
+
+const { protect, authorize } = require('../middleware/auth');
 
 router.route('/').get(
   advancedResults(Course, {
@@ -17,7 +20,7 @@ router.route('/').get(
     select: 'name description'
   }),
   getCourses
-).post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+).post(protect, authorize('publisher','admin'),  addCourse);
+router.route('/:id').get(getCourse).put(protect, authorize('publisher','admin'), updateCourse).delete(protect, authorize('publisher','admin'), deleteCourse);
 
 module.exports = router;
